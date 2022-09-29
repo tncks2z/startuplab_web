879303c6ed7ddd86741a4f70438d5937ef38075a
suchan <limsuchan@startuplab.team>
2022-09-29 13:13:39 +0900

    beautify

--- src/views/AdminModifyData.vue
+++ src/views/AdminModifyData.vue
@@ -1,29 +1,38 @@
 <template>
-  <div class="container">
-    <div>
-      <h3>{{ projectName }}</h3>
-    </div>
-    <form class="needs-validation" @submit.prevent="submitForm($event)" novalidate>
-      <div v-for="(column, i) in columnList.data" :key="i">
-        <UserInput :label="column.meta_name" :inputValue="(inputValueList[column.meta_name] = inputValueList[column.meta_name])" @inputFromChild="inputValueList[column.meta_name] = $event.target.value" v-if="column.meta_type === '1'" />
-        <UserSelectBox
-          :label="column.meta_name"
-          :selectValue="(inputValueList[column.meta_name] = inputValueList[column.meta_name])"
-          @selectFromChild="inputValueList[column.meta_name] = Number($event.target.value)"
-          v-else-if="column.meta_type === '2'"
-        />
-        <UserNote :label="column.meta_name" :note="(inputValueList[column.meta_name] = inputValueList[column.meta_name])" @inputFromChild="inputValueList[column.meta_name] = $event.target.value" v-else-if="column.meta_type === '5'" />
-        <UserRadioBox
-          :label="column.meta_name"
-          :radioValue="(inputValueList[column.meta_name] = inputValueList[column.meta_name])"
-          @radioFromChild="inputValueList[column.meta_name] = Number($event.target.value)"
-          v-else-if="column.meta_type === '4'"
-        />
-      </div>
-      <UserRadioBox :radioValue="(data_status = data_status)" @radioFromChild="changeStatusValue($event)" v-if="data_status != 1 && data_status != 2" />
-      <button type="submit" class="btn btn-secondary">저장</button>
-    </form>
-  </div>
+	<div class="container">
+		<div>
+			<h3>{{ projectName }}</h3>
+		</div>
+		<form class="needs-validation" @submit.prevent="submitForm($event)" novalidate>
+			<div v-for="(column, i) in columnList.data" :key="i">
+				<UserInput
+					:label="column.meta_name"
+					:inputValue="(inputValueList[column.meta_name] = inputValueList[column.meta_name])"
+					@inputFromChild="inputValueList[column.meta_name] = $event.target.value"
+					v-if="column.meta_type === '1'" />
+				<UserSelectBox
+					:label="column.meta_name"
+					:selectValue="(inputValueList[column.meta_name] = inputValueList[column.meta_name])"
+					@selectFromChild="inputValueList[column.meta_name] = Number($event.target.value)"
+					v-else-if="column.meta_type === '2'" />
+				<UserNote
+					:label="column.meta_name"
+					:note="(inputValueList[column.meta_name] = inputValueList[column.meta_name])"
+					@inputFromChild="inputValueList[column.meta_name] = $event.target.value"
+					v-else-if="column.meta_type === '5'" />
+				<UserRadioBox
+					:label="column.meta_name"
+					:radioValue="(inputValueList[column.meta_name] = inputValueList[column.meta_name])"
+					@radioFromChild="inputValueList[column.meta_name] = Number($event.target.value)"
+					v-else-if="column.meta_type === '4'" />
+			</div>
+			<UserRadioBox
+				:radioValue="(data_status = data_status)"
+				@radioFromChild="changeStatusValue($event)"
+				v-if="data_status != 1 && data_status != 2" />
+			<button type="submit" class="btn btn-secondary">저장</button>
+		</form>
+	</div>
 </template>
 
 <script>
@@ -36,153 +45,153 @@ import UserNote from '/@components/UserNote.vue';
 import { getUserEditForm, getUserAddForm } from '/@service/user';
 
 export default {
-  data() {
-    return {
-      inputValueList: {},
-      columnList: [],
-      errors: {
-        form: [],
-        status: [],
-      },
-      projectName: '',
-      projectCode: '',
-      data_id: '',
-      data_status: 0,
-      isPassValidatoin: false,
-    };
-  },
-  created() {
-    const setAddData = new FormData();
-    const setEditData = new FormData();
+	data() {
+		return {
+			inputValueList: {},
+			columnList: [],
+			errors: {
+				form: [],
+				status: [],
+			},
+			projectName: '',
+			projectCode: '',
+			data_id: '',
+			data_status: 0,
+			isPassValidatoin: false,
+		};
+	},
+	created() {
+		const setAddData = new FormData();
+		const setEditData = new FormData();
 
-    this.projectCode = sessionStorage.getItem('selectedWork');
-    this.projectName = sessionStorage.getItem('workName');
-    this.data_id = sessionStorage.getItem('data_id');
+		this.projectCode = sessionStorage.getItem('selectedWork');
+		this.projectName = sessionStorage.getItem('workName');
+		this.data_id = sessionStorage.getItem('data_id');
 
-    setAddData.set('work_id', this.projectCode);
-    setEditData.set('data_id', this.data_id);
+		setAddData.set('work_id', this.projectCode);
+		setEditData.set('data_id', this.data_id);
 
-    getUserAddForm(setAddData).then((result) => {
-      this.columnList = result.data;
-    });
+		getUserAddForm(setAddData).then((result) => {
+			this.columnList = result.data;
+		});
 
-    getUserEditForm(setEditData).then((result) => {
-      this.dataList = result.data;
-      this.data_status = this.dataList.data.data_status;
-      this.inputValueList = JSON.parse(this.dataList.data.data_json);
-      console.log(this.data_status);
-    });
-  },
-  methods: {
-    changeStatusValue(e) {
-      this.data_status = Number(e.target.value);
-      if (this.data_status != 2 && this.data_status != 6) {
-        var forms = document.querySelectorAll('.needs-validation');
-        Array.prototype.slice.call(forms).forEach(function (form) {
-          form.classList.remove('was-validated');
-        });
-      }
-    },
-    checkForm() {
-      this.errors = {
-        form: [],
-        status: [],
-      };
-      for (var key in this.inputValueList) {
-        if (this.inputValueList[key] === '') {
-          this.inputValueList[key] = null;
-          this.errors['form'].push('Error');
-        }
-      }
-      // if (this.data_status == 1 || this.data_status == 2) {
-      // 	this.errors['status'].push('Error');
-      // }
-      if (this.errors['form'].length != 0 && this.data_status == 6) {
-        alert('모든 입력창을 채워주세요');
-        var forms = document.querySelectorAll('.needs-validation');
-        Array.prototype.slice.call(forms).forEach(function (form) {
-          form.classList.add('was-validated');
-        });
-        // } else if (this.errors['status'].length != 0) {
-        // 	alert('데이터 상태에 체크를 해주세요');
-        // 	var forms = document.querySelectorAll('.needs-validation');
-        // 	Array.prototype.slice.call(forms).forEach(function (form) {
-        // 		form.classList.add('was-validated');
-        // 	});
-      } else {
-        this.isPassValidatoin = true;
-      }
-    },
-    submitForm(e) {
-      this.checkForm();
-      if (this.isPassValidatoin) {
-        var inputData = JSON.stringify(this.inputValueList);
-        axios({
-          method: 'post',
-          url: 'http://52.22.216.42:8090/web/db/store',
-          data: {
-            work_id: 10,
-            user_id: 2,
-            data_json: inputData,
-            data_status: this.data_status,
-          },
-        })
-          .then((res) => {
-            console.log(inputData, this.data_status);
-            e.target.reset();
-            this.inputValueList = {};
-            var forms = document.querySelectorAll('.needs-validation');
-            Array.prototype.slice.call(forms).forEach(function (form) {
-              form.classList.remove('was-validated');
-            });
-            this.$router.go(-1);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
-    },
-  },
-  components: { UserInput, Address, UserSelectBox, UserRadioBox, UserNote },
+		getUserEditForm(setEditData).then((result) => {
+			this.dataList = result.data;
+			this.data_status = this.dataList.data.data_status;
+			this.inputValueList = JSON.parse(this.dataList.data.data_json);
+			console.log(this.data_status);
+		});
+	},
+	methods: {
+		changeStatusValue(e) {
+			this.data_status = Number(e.target.value);
+			if (this.data_status != 2 && this.data_status != 6) {
+				var forms = document.querySelectorAll('.needs-validation');
+				Array.prototype.slice.call(forms).forEach(function (form) {
+					form.classList.remove('was-validated');
+				});
+			}
+		},
+		checkForm() {
+			this.errors = {
+				form: [],
+				status: [],
+			};
+			for (var key in this.inputValueList) {
+				if (this.inputValueList[key] === '') {
+					this.inputValueList[key] = null;
+					this.errors['form'].push('Error');
+				}
+			}
+			// if (this.data_status == 1 || this.data_status == 2) {
+			// 	this.errors['status'].push('Error');
+			// }
+			if (this.errors['form'].length != 0 && this.data_status == 6) {
+				alert('모든 입력창을 채워주세요');
+				var forms = document.querySelectorAll('.needs-validation');
+				Array.prototype.slice.call(forms).forEach(function (form) {
+					form.classList.add('was-validated');
+				});
+				// } else if (this.errors['status'].length != 0) {
+				// 	alert('데이터 상태에 체크를 해주세요');
+				// 	var forms = document.querySelectorAll('.needs-validation');
+				// 	Array.prototype.slice.call(forms).forEach(function (form) {
+				// 		form.classList.add('was-validated');
+				// 	});
+			} else {
+				this.isPassValidatoin = true;
+			}
+		},
+		submitForm(e) {
+			this.checkForm();
+			if (this.isPassValidatoin) {
+				var inputData = JSON.stringify(this.inputValueList);
+				axios({
+					method: 'post',
+					url: 'http://52.22.216.42:8090/web/db/store',
+					data: {
+						work_id: 10,
+						user_id: 2,
+						data_json: inputData,
+						data_status: this.data_status,
+					},
+				})
+					.then((res) => {
+						console.log(inputData, this.data_status);
+						e.target.reset();
+						this.inputValueList = {};
+						var forms = document.querySelectorAll('.needs-validation');
+						Array.prototype.slice.call(forms).forEach(function (form) {
+							form.classList.remove('was-validated');
+						});
+						this.$router.go(-1);
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			}
+		},
+	},
+	components: { UserInput, Address, UserSelectBox, UserRadioBox, UserNote },
 };
 </script>
 
 <style scoped>
 .container {
-  margin-top: 20px;
+	margin-top: 20px;
 }
 h3 {
-  margin: 50px;
-  text-align: center;
+	margin: 50px;
+	text-align: center;
 }
 .btn-secondary {
-  display: flex;
-  justify-content: center;
-  margin: 30px auto;
-  font-size: 0.9rem;
-  border: none;
-  width: 15%;
-  background-color: #e17b46;
+	display: flex;
+	justify-content: center;
+	margin: 30px auto;
+	font-size: 0.9rem;
+	border: none;
+	width: 15%;
+	background-color: #e17b46;
 }
 .btn-secondary:hover {
-  background-color: #c15a33;
+	background-color: #c15a33;
 }
 @media (max-width: 768px) {
-  h3 {
-    margin: 50px;
-    text-align: center;
-  }
-  .btn {
-    display: flex;
-    justify-content: center;
-    margin: 30px auto;
-    font-size: 0.9rem;
-    border: none;
-    width: 20%;
-    background-color: #e17b46;
-  }
-  .btn-secondary:hover {
-    background-color: #c15a33;
-  }
+	h3 {
+		margin: 50px;
+		text-align: center;
+	}
+	.btn {
+		display: flex;
+		justify-content: center;
+		margin: 30px auto;
+		font-size: 0.9rem;
+		border: none;
+		width: 20%;
+		background-color: #e17b46;
+	}
+	.btn-secondary:hover {
+		background-color: #c15a33;
+	}
 }
 </style>
